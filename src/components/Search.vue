7d2a8b93994d98e720b135eaaf9af1b24bdba228
33357 <1043898730@qq.com>
2022-04-16 11:06:05 +0100

    0.0.1

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -10,31 +10,13 @@
         :not-found-content="null"
         @search="handleSearch"
       >
-        <a-select-option :value="searchData.address">
-          <div v-if="searchData.type == 'erc20'" @click="selectChat(searchData.address)">
-            <div
-              class="avatar"
-            >
-              <my-avatar :avatar="appAsync.erc20DetailMap[searchData.address].value.logoURI" :canCheck="false"></my-avatar>
-              <span class="avatar-name">{{
-                appAsync.erc20DetailMap[searchData.address].value.name +
-                  ' (' +
-                  utils.format.balance(
-                    appAsync.tokenBalanceMap[searchData.address][appSync.userAddress].value,
-                    appAsync.erc20DetailMap[searchData.address].value.decimals,
-                    appAsync.erc20DetailMap[searchData.address].value.symbol,
-                    appStorage.decimalLimit
-                  ) +
-                  ')'
-              }}</span>
+        <a-select-option v-if="searchData.recipientText" :value="searchData.recipientText">
+          <div @click="selectChat(searchData.recipientText)">
+            <div class="avatar">
+              <my-avatar :avatar="appSync.avatarMap[searchData.recipientHash]" :canCheck="false"></my-avatar>
+              <span class="avatar-name">{{ `${searchData.recipientText} 消息：${searchData.recipientMessageIdLength}` }}</span>
             </div>
           </div>
-          <div v-else-if="searchData.type == 'wallet'" class="avatar" @click="selectChat(searchData.address)">
-            <my-avatar :avatar="appSync.addressAvatarMap[searchData.address]" :canCheck="false"></my-avatar>
-            <span class="avatar-name">{{ searchData.address }}</span>
-          </div>
-          <div v-else-if="searchData.err">{{ searchData.err }}</div>
-          <div v-else @click="selectChat(searchData.address)">{{ $t('search.unknown_contract_address') }}</div>
         </a-select-option>
       </a-select>
     </div>
@@ -45,7 +27,7 @@
 import { Component, Vue } from 'vue-property-decorator';
 import { namespace } from 'vuex-class';
 import MyAvatar from '@/components/Avatar.vue';
-import { utils, etherUtils } from '@/const';
+import { utils } from '@/const';
 import { AppStorage, AppSync, AppAsync, ChatSync } from '@/store';
 const chatModule = namespace('chat');
 const appModule = namespace('app');
@@ -61,40 +43,22 @@ export default class MySearch extends Vue {
   @appModule.State('async') appAsync: AppAsync;
   @chatModule.State('sync') chatSync: ChatSync;
 
-  searchData: { address?: string; type?: string; err?: string } = {};
+  searchData: { recipientText?: string; recipientHash?: string; recipientMessageIdLength?: number } = {};
   utils = utils;
 
-  async handleSearch(value: string) {
-    if (etherUtils.isAddress(value)) {
-      try {
-        const address = etherUtils.getAddress(value);
-        const type = await this.$store.state.app.sync.ether.utils.getType(address);
-        if (type == 'erc20') {
-          await this.$store.dispatch('app/setTokenBalance', [address, this.appSync.userAddress]);
-          await this.$store.dispatch('app/setERC20Detail', address);
-        } else if (type == 'wallet') {
-          await this.$store.dispatch('app/setAddressAvatar', address);
-        }
-        this.searchData = {
-          address,
-          type,
-        };
-        // eslint-disable-next-line prettier/prettier
-      } catch (err:any) {
-        console.log(err);
-        this.searchData = {
-          err: err.message,
-        };
-      }
-    } else {
-      this.searchData = {
-        err: this.$t('search.not_ether_address').toString(),
-      };
-    }
+  async handleSearch(recipientText: string) {
+    const recipientHash = (await this.appSync.ether.getBlockChat().getRecipientHash(recipientText)).toString();
+    await this.$store.dispatch('app/setAvatar', recipientHash);
+    const recipientMessageIdLength = (await this.appSync.ether.getBlockChat().getRecipientMessageListLength(recipientHash)).toNumber();
+    this.searchData = {
+      recipientText,
+      recipientHash,
+      recipientMessageIdLength,
+    };
   }
 
-  async selectChat(address: string) {
-    await this.$store.dispatch('chat/setUserActiveRecipient', address);
+  async selectChat(recipientText: string) {
+    await this.$store.dispatch('chat/setActiveRecipient', recipientText);
     this.searchData = {};
   }
 }
